06bdd9b0a1f43df3ab9367972a1248aabf73a073
= <=>
2026-04-07 23:40:49 +0530

    updated instacen type

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "secodary_vpc_cidr" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "primary_key_name" {
